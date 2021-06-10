ad386a6069d1cbd177ab4e48e4c03f3490ab301a
a-marii <chuprina.maria2015@yandex.ru>
2021-06-10 21:47:29 +0300

    final

--- history_quiz-frontend/src/APIHelper.js
+++ history_quiz-frontend/src/APIHelper.js
@@ -2,30 +2,10 @@ import axios from "axios"
 
 const API_URL = "https://history-quiz-1.herokuapp.com/todos/"
 
-async function createTodo(task, completed1,completed2,completed3,completed4 ) {
-  const { data: newTodo } = await axios.post(API_URL, {
-    task,
-    completed1,
-    completed2,
-    completed3,
-    completed4
-  })
-  return newTodo
-}
-
-async function deleteTodo(id) {
-  const message = await axios.delete(`${API_URL}${id}`)
-  return message
-}
-
-async function updateTodo(id, payload) {
-  const { data: newTodo } = await axios.put(`${API_URL}${id}`, payload)
-  return newTodo
-}
 
 async function getAllQuestion() {
   const { data: todos } = await axios.get(API_URL)
   return todos
 }
 
-export default { createTodo, deleteTodo, updateTodo, getAllQuestion }
+export default { getAllQuestion }
